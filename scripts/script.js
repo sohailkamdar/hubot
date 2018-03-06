3a6036e1f756a0b6103295e1f0683d0e410333c7
sohailkamdar <sohailk@youthtruthsurvey.org>
2018-03-05 16:50:52 -0800

    snotty bot now tracks your pantry

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,86 @@
-// Description:
-//
-//
-// Dependencies:
-//   None
-//
-// Configuration:
-//   None
-//
-// Commands:
-//
+// module.exports = function(robot) {
+//   let trigger = /(?:eta)(?: to)? (.*) (?: from)? (.*)? (?:by) (.*)/i;
+
+//   robot.respond(trigger, function(res){
+//     const key = "AIzaSyAR_br69wgTxLYgKh4I34HJTTNvi8tUABg";
+    // let origins = res.match[1];
+    // origins = origins.split(' ').join('+');
+    // let destination = res.match[2];
+    // destination = destination.split(' ').join('+');
+    // let mode = res.match[3];
+//     if (mode === 'car'){
+//       mode= 'driving'
+//     }else if (mode === 'bike'){
+//       mode = 'biking'
+//     }else if (mode === 'walk'){
+//       mode = 'walking'
+//     };
+//     let Url = makeURL(origins,destination,mode,key);
+//     console.log(Url);
+//     return robot.http(Url)
+//         .header('Accept', 'application/json')
+//         .get() (function (err,res, body){
+//           let data
+//           try { 
+//             data = JSON.parse(body)
+//             console.log(data)}
+//           catch(err)
+//           {res.send("Ran into an error parsing JSON :(")}
+//           return console.log(data)
+//           let time = timeToDestination(data)
+//           return res.reply(time)
+//           });
+//         });
+      
+
+// function makeURL(origins, destination, mode, key){
+//   let url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins='+ origins + '&destinations=' + destination+ '&mode='+mode+'&language=fr-FR&key=' + key
+//   return url
+//   }
+  
+//   function timeToDestination(json_obj) {
+//     if ( json_obj.rows[0].elements[0].status !=='OK') {
+//       return('error');
+//     } else {
+//       var origin = response.originAddresses[0];
+//       var destination = response.destinationAddresses[0];
+//       if (json_obj.rows[0].elements[0].status === "ZERO_RESULTS") {
+//         return("There are no roads between " 
+//                           + origin + " and " + destination);
+//       } else {
+//         var duration = response.rows[0].elements[0].duration;
+//         var duration_text = duration.text;
+//         return ("It will take "+ duration_text+" to get from " +origin+ " to "+ destination);
+//       }
+//     }
+//   }
+// }
 
 module.exports = function(robot) {
-  //  YOUR CODE HERE
-  //  Example
-   robot.hear(/javascript/i, function(msg) {
-     return msg.send("I love writing code!");
-   });
+  let pantry = ['apple','chips', 'crackers', 'popcorn', 'cereal','chocolate','bananas']
+  robot.respond(/bought a (.*) for the pantry/i, function(msg){
+    let newFood = msg.match[1]
+    pantry.push()
+    return msg.send('added a '+newFood +' to the pantry')
+  }),
+  robot.respond(/get me a snack/i, function(msg){
+    let food = pantry[Math.floor(Math.random() * pantry.length)]
+    let foodIndex = pantry.indexOf(food)
+    if (foodIndex > -1){
+      array.splice(index, 1)
+    }
+    return msg.send('Here is a'+ food)
+  }),
+  robot.respond(/what is in the pantry/i, function(msg){
+     
+    return msg.send('You have: ' + pantry.toString())
+  }),
+  robot.respond(/I ate a (.*)/i, function(msg){
+    let foodEaten = msg.match[1]
+    let foodEatenIndex = pantry.indexOf(foodEaten)
+    if (foodEatenIndex > -1){
+      array.splice(index, 1)
+    }
+    return msg.send('removing '+ foodEaten+ ' from the pantry')
+  })
 }
-
-/************************************
-
-EXAMPLES OF THE KEY HUBOT FUNCTIONS
-
-************************************/
-
-/* Variables for random example */
-
-// const squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
-
-module.exports = function(robot) {
-  /* Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" */
-  robot.hear(/hi|hello/i, function(msg) {
-    return msg.send("Hi there!");
-  });
-}
-  robot.respond(/what is the time/i, function(msg){
-    let when = new Date()
-    return msg.send(when.toLocaleTimeString('en-US'))
-  });
-// };
